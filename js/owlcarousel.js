a591aa70c25c0be5c3c469ffc9206d9a881fdc0d
almamunxd <almamunxd@gmail.com>
2022-11-21 22:48:59 +0600

    first commit

---
+++ js/owlcarousel.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    margin: 15,
+    nav: true,
+    dots: false,
+    navText: [
+        "<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
